8eb2087213b5c559361d0285c99925be4811e754
Sautino <marcelsauter9299@gmail.com>
2021-01-21 10:54:30 +0100

    initial commit

---
+++ src/main/java/com/ayc/ayccompanyservice/AycCompanyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ayc.ayccompanyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AycCompanyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AycCompanyServiceApplication.class, args);
+	}
+
+}
